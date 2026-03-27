4faa18c5bb8191e4f91e0c4ed64786bacd8c9799
Jingshi Liu <jingshiliu07@gmail.com>
2026-03-27 02:54:36 -0600

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import './App.css'
 
 function App() {
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-            <h1 className="text-4xl font-bold">Hello Vite + React + Tailwind!</h1>
+        <div className="flex items-start justify-start h-screen bg-gray-900 text-white">
+            <h1 className="text-4xl font- ">Hello Vite + Resact + Tailwind!</h1>
+            <h2>Hello Wsorld!</h2>
         </div>
     );
 }
