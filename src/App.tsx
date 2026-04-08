e69c876e55f72f050bab017934f759fa58f37c19
Jingshi Liu <jingshiliu07@gmail.com>
2026-04-07 21:55:47 -0600

    update configs to fix tailwind not working

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import './App.css'
 
 function App() {
     return (
-        <div className="flex items-start justify-start h-screen bg-gray-900 text-white">
-            <h1 className="text-4xl font- ">Hello Vite + Resact + Tailwind!</h1>
-            <h2>Hello Wsorld!</h2>
+        <div className="flex items-start justify-start h-screen">
+            <h1 className="text-4xl">Hello Vite + React + Tailwind!</h1>
         </div>
     );
 }
